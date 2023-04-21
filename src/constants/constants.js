5458e90f7ec0bf9482def4162b3c63c441946239
pritom <pritom@pop-os.localdomain>
2023-04-21 13:33:40 +0600

    edit constant domain for build .1 vercel

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 // const DOMAIN = "http://localhost:5000";
-const DOMAIN = `https://contact-manager-j80n.onrender.com`;
+const DOMAIN = `https://contact-manager-fullstack.onrender.com`;
 const GET_ALL_CONTACTS_URL = `${DOMAIN}/api/contacts`;
 const POST_NEW_CONTACT_URL = `${DOMAIN}/api/contacts`;
 const PUT_EDIT_CONTACT_URL_fn = (id) => {
